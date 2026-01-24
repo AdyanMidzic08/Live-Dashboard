ec50ba5e91b0bae73c3fcd04d7cb6b73c7577a3f
Adyan <78735036+AdyanMidzic08@users.noreply.github.com>
2026-01-24 16:34:08 +0100

    Server Setup + POST & GET Method for todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="container mt-5">
+      <h1 className="text-primary">Bootstrap is working!</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
